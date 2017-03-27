9036416847986eb64d49cb5acf4c93af25400f35
Laura Turk <laura.lk.turk@gmail.com>
2017-03-26 21:03:04 -0600

    Complete Hello World

---
+++ ecmascript/hello-world/hello-world.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  hello(string = 'Hello, World!') {
+    return `Hello, ${string}!`
+  }
+}
+
+export default HelloWorld;
